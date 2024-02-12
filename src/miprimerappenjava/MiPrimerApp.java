a7182456056b696a95118621358897076766348d
Bysarc <159395421+Bysarc@users.noreply.github.com>
2024-02-12 12:59:14 -0500

    Primer commit

---
+++ src/miprimerappenjava/MiPrimerApp.java
@@ -0,0 +1,12 @@
+
+package miprimerappenjava;
+
+/**
+ *
+ * @author SERGIO ROJAS
+ */
+public class MiPrimerApp {
+    public static void main(String[] args) {
+          System.out.println("hola mundo"); //se ejecuta o se imprime el mensaje "hola mundo"
+    }
+}
